06c428fead0f41f3b6d0351ed1c3032070291783
Your Name <you@example.com>
2020-02-21 22:34:09 +0100

    dodana walidacja

--- react_blog/src/Formularz/form.js
+++ react_blog/src/Formularz/form.js
@@ -6,8 +6,48 @@ export default class Form extends React.Component {
         this.state = {
             id: null,
             title: "",
-            content: ""
+            content: "",
+            errormessage: ""
+           
         };
+    }
+
+    myChangeHandler = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+        let errId = '';
+        let errTitle = '';
+        let errContent = '';
+
+
+        if (name === "id") {
+            if (value !="" && !Number(value)) {
+                errId = <strong>Your ID must be a number</strong>;
+
+            }
+            this.setState({errormessage: errId});
+        }
+        
+    
+
+        if (name === "title"){
+            if (value.length <= 2){
+                errTitle = <strong>Your title must be more than 2 characters</strong>;
+            }  
+            this.setState({errormessage: errTitle});
+        }
+
+        
+        if (name === "content"){
+            if (value.length <= 100){
+                errContent = <strong>Your content must be more than 100 characters</strong>;
+            }  
+            this.setState({errormessage: errContent});
+        }
+
+       
+
+        this.setState({[name]: value});
     }
 
     render() {
@@ -17,22 +57,29 @@ export default class Form extends React.Component {
                 <input
                     type='text'
                     name='id'
+                    onChange={this.myChangeHandler}
                 />
                 <p>Please enter title:</p>
                 <input
                     type='text'
                     name='title'
+                    onChange={this.myChangeHandler}
                 />
                 <p>Please enter content:</p>
                 <textarea className="text-area"
                     type='text'
                     name='content'
+                    onChange={this.myChangeHandler}
                 />
                     <br/><br/>
 
                     <button className="submitButton">Submit</button>
+
+                    <h4>{this.state.errormessage}</h4>
+
             </form>
 
+                        
         );
     }
 }
