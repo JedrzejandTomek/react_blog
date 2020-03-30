04ea76421e9e5864420f2e5706fe5e6e65fd58ec
Your Name <you@example.com>
2020-03-30 22:03:08 +0200

    dodane cookies po stronie serwera i uzytkownika

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,33 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 let dbConfig = require('./db/db');
+var cookieParser = require('cookie-parser')
+
+
 
 const articleRoute = require('./routes/article.route');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+
+
 app.use(cors());
 app.use(express.json());
 app.use('/articles', articleRoute);
+
+app.use(cookieParser());
+app.get('/home', function(req, res) {
+    
+    if(req.cookies.count){
+        var count = parseInt(req.cookies.count);
+    } else {
+        var count = 0;
+    }
+    count = count+1;
+    res.cookie('count', count);
+    res.send('count :'+ count);
+});
 
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.db, {
--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,57 @@
 import React from 'react'
 
+
+function setCookie(cookieName, cookieValue) {
+
+   
+    document.cookie = cookieName + "=" + cookieValue;
+};
+
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+
+    var ca = decodedCookie.split(";");
+
+    for (var i = 0; i < ca.length; i++) {
+        var cookie = ca[i];
+        while (cookie.charAt(0) === " ") {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+
+    var counter=getCookie("Visits");
+    if (counter === null){
+        counter = 1;
+    } else {
+
+            counter ++;
+            setCookie("Visits", counter);
+        }
+    }
+
+getCookie();
+checkCookie();
+
 export default class Home extends React.Component {
+       
+      
 
     render(){
+               
         return(
             <div>
             <h1>Artykuły</h1>
+           
             </div>
+            
         )
     }
 }
