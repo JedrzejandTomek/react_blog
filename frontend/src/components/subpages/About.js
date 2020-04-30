7d62293aafc471e72b6837dba0b45188a9e8b2cd
Your Name <you@example.com>
2020-04-30 23:13:49 +0200

    prawie finał

--- frontend/src/components/subpages/About.js
+++ frontend/src/components/subpages/About.js
@@ -3,10 +3,23 @@ import React from "react";
 export default function About() {
 
         return (
-
-            <div>
-               <h2>Blog made by Jedrzej and Tomek &copy;</h2>
+            
+<div >
+    <div className="content-box">
+        <div className="image-container">
+            <div className="image-box">
+                <img src={require("./pics/utah.jpg")}/>
             </div>
+
+             <div className="image-box">
+                <img src={require("./pics/maledives.jpg")}/>
+            </div>
+        </div>
+    </div>  
+    <div className="made">
+    <h2>Blog made by Jedrzej and Tomek &copy;</h2>  
+    </div>
+</div>
     );
     
 
